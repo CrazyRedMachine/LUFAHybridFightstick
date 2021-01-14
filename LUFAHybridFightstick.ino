6ad979f7c506da270e6f948b4c3ef20124364dee
CrazyRedMachine <crazyredmachine86@gmail.com>
2021-01-14 01:16:38 +0100

    redefined pinout

--- LUFAHybridFightstick.ino
+++ LUFAHybridFightstick.ino
@@ -6,8 +6,27 @@
 #include <EEPROM.h>
 #include <inttypes.h>
 
-//delay in ms for start+select to become HOME
+//make it so holding start+select triggers the HOME button
+//#define HOME_HOTKEY
+//delay in ms for start+select to become HOME in HOME_HOTKEY mode
 #define HOME_DELAY 1000
+
+/* PINOUT (follows Nintendo naming (X=up, B=down)) */
+#define PIN_UP    A0
+#define PIN_DOWN  A2
+#define PIN_LEFT  A1
+#define PIN_RIGHT A3
+#define PIN_A     5            //XBOX B
+#define PIN_B     4            //XBOX A
+#define PIN_X     3            //XBOX Y
+#define PIN_Y     15           //XBOX X
+#define PIN_L     14           //XBOX LB
+#define PIN_R     2            //XBOX RB
+#define PIN_ZL    6            //XBOX LT
+#define PIN_ZR    7            //XBOX RT
+#define PIN_PLUS  16           //XBOX START
+#define PIN_MINUS 10           //XBOX BACK
+#define PIN_HOME  9
 
 /* Buttons declarations */
 #define MILLIDEBOUNCE 1 //Debounce time in milliseconds
@@ -25,12 +44,12 @@ Bounce buttonA = Bounce();
 Bounce buttonB = Bounce();
 Bounce buttonX = Bounce();
 Bounce buttonY = Bounce();
-Bounce buttonLB = Bounce();
-Bounce buttonRB = Bounce();
-Bounce buttonLT = Bounce();
-Bounce buttonRT = Bounce();
-Bounce buttonSTART = Bounce();
-Bounce buttonSELECT = Bounce();
+Bounce buttonL = Bounce();
+Bounce buttonR = Bounce();
+Bounce buttonZL = Bounce();
+Bounce buttonZR = Bounce();
+Bounce buttonPLUS = Bounce();
+Bounce buttonMINUS = Bounce();
 Bounce buttonHOME = Bounce();
 
 /* MODE DECLARATIONS */
@@ -70,21 +89,21 @@ void checkModeChange(){
 }
 
 void setupPins(){
-    joystickUP.attach(A0,INPUT_PULLUP);
-    joystickDOWN.attach(A2,INPUT_PULLUP);
-    joystickLEFT.attach(A1,INPUT_PULLUP);
-    joystickRIGHT.attach(A3,INPUT_PULLUP);
-    buttonA.attach(5,INPUT_PULLUP);
-    buttonB.attach(4,INPUT_PULLUP);;
-    buttonX.attach(3,INPUT_PULLUP);
-    buttonY.attach(15,INPUT_PULLUP);
-    buttonLB.attach(14,INPUT_PULLUP);
-    buttonRB.attach(2,INPUT_PULLUP);
-    buttonLT.attach(6,INPUT_PULLUP);
-    buttonRT.attach(7,INPUT_PULLUP);
-    buttonSTART.attach(16,INPUT_PULLUP);
-    buttonSELECT.attach(10,INPUT_PULLUP);
-    buttonHOME.attach(9,INPUT_PULLUP);
+    joystickUP.attach(PIN_UP,INPUT_PULLUP);
+    joystickDOWN.attach(PIN_DOWN,INPUT_PULLUP);
+    joystickLEFT.attach(PIN_LEFT,INPUT_PULLUP);
+    joystickRIGHT.attach(PIN_RIGHT,INPUT_PULLUP);
+    buttonA.attach(PIN_A,INPUT_PULLUP);      // XBOX B
+    buttonB.attach(PIN_B,INPUT_PULLUP);      // XBOX A
+    buttonX.attach(PIN_X,INPUT_PULLUP);      // XBOX Y
+    buttonY.attach(PIN_Y,INPUT_PULLUP);     // XBOX X
+    buttonL.attach(PIN_L,INPUT_PULLUP);     // XBOX LB
+    buttonR.attach(PIN_R,INPUT_PULLUP);      // XBOX RB
+    buttonZL.attach(PIN_ZL,INPUT_PULLUP);     // XBOX LT
+    buttonZR.attach(PIN_ZR,INPUT_PULLUP);     // XBOX RT
+    buttonPLUS.attach(PIN_PLUS,INPUT_PULLUP);  // XBOX START
+    buttonMINUS.attach(PIN_MINUS,INPUT_PULLUP); // XBOX BACK
+    buttonHOME.attach(PIN_HOME,INPUT_PULLUP);
 
     joystickUP.interval(MILLIDEBOUNCE);
     joystickDOWN.interval(MILLIDEBOUNCE);
@@ -94,12 +113,12 @@ void setupPins(){
     buttonB.interval(MILLIDEBOUNCE);
     buttonX.interval(MILLIDEBOUNCE);
     buttonY.interval(MILLIDEBOUNCE);
-    buttonLB.interval(MILLIDEBOUNCE);
-    buttonRB.interval(MILLIDEBOUNCE);
-    buttonLT.interval(MILLIDEBOUNCE);
-    buttonRT.interval(MILLIDEBOUNCE);
-    buttonSTART.interval(MILLIDEBOUNCE);
-    buttonSELECT.interval(MILLIDEBOUNCE);
+    buttonL.interval(MILLIDEBOUNCE);
+    buttonR.interval(MILLIDEBOUNCE);
+    buttonZL.interval(MILLIDEBOUNCE);
+    buttonZR.interval(MILLIDEBOUNCE);
+    buttonPLUS.interval(MILLIDEBOUNCE);
+    buttonMINUS.interval(MILLIDEBOUNCE);
     buttonHOME.interval(MILLIDEBOUNCE);
 }
 void setup() {
@@ -110,7 +129,7 @@ void setup() {
   setupPins();
   delay(500);
 // if select is held on boot, NSWitch mode
-  int value = digitalRead(10);
+  int value = digitalRead(PIN_MINUS);
   if (value == LOW)
   {
     xinput = false;
@@ -118,7 +137,7 @@ void setup() {
   }
 // if start is held on boot, XInput mode
   else {
-    value = digitalRead(16);
+    value = digitalRead(PIN_PLUS);
     if (value == LOW)
       {
         xinput = true;
@@ -206,15 +225,14 @@ void buttonRead()
   if (buttonB.update()) {buttonStatus[BUTTONB] = buttonB.fell();}
   if (buttonX.update()) {buttonStatus[BUTTONX] = buttonX.fell();}
   if (buttonY.update()) {buttonStatus[BUTTONY] = buttonY.fell();}
-  if (buttonLB.update()) {buttonStatus[BUTTONLB] = buttonLB.fell();}
-  if (buttonRB.update()) {buttonStatus[BUTTONRB] = buttonRB.fell();}
-  if (buttonLT.update()) {buttonStatus[BUTTONLT] = buttonLT.fell();}
-  if (buttonRT.update()) {buttonStatus[BUTTONRT] = buttonRT.fell();}
-  if (buttonSTART.update()) {buttonStatus[BUTTONSTART] = buttonSTART.fell();}
-  if (buttonSELECT.update()) {buttonStatus[BUTTONSELECT] = buttonSELECT.fell();}
+  if (buttonL.update()) {buttonStatus[BUTTONLB] = buttonL.fell();}
+  if (buttonR.update()) {buttonStatus[BUTTONRB] = buttonR.fell();}
+  if (buttonZL.update()) {buttonStatus[BUTTONLT] = buttonZL.fell();}
+  if (buttonZR.update()) {buttonStatus[BUTTONRT] = buttonZR.fell();}
+  if (buttonPLUS.update()) {buttonStatus[BUTTONSTART] = buttonPLUS.fell();}
+  if (buttonMINUS.update()) {buttonStatus[BUTTONSELECT] = buttonMINUS.fell();}
   if (buttonHOME.update()) { buttonStatus[BUTTONHOME] = buttonHOME.fell();}
 
-#define HOME_HOTKEY
 #ifdef HOME_HOTKEY  
   if(buttonStatus[BUTTONSTART] && buttonStatus[BUTTONSELECT]) {
    if (startAndSelTime == 0)
